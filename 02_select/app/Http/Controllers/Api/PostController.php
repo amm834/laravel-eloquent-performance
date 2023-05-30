f9cac8b1d0c06aaa2620e714673a441ea82c51ec
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-30 21:04:25 +0630

    feat: add reudce transfer data size in API level

---
+++ 02_select/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::select('user_id', 'title', 'body')
+            ->with('user:id,name,email')
+            ->take(100)
+            ->get();
+        return response()->json(PostResource::collection($posts));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- 02_select/database/seeders/DatabaseSeeder.php
+++ 02_select/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        Post::factory(30)->create();
+        Post::factory(10000)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
